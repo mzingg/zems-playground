ae8b0b45854ea603817fb2e7e7ea64f3ace173e5
Markus Zingg <markus.zingg@gmail.com>
2021-06-07 08:15:07 +0200

    Adds contentbus support to the page title

--- app/src/main/components/zems/playground/PageTitle/index.mjs
+++ app/src/main/components/zems/playground/PageTitle/index.mjs
@@ -1,9 +1,10 @@
 import {jsxLight} from '../../../../modules/zems/core/React/index.mjs'; /*$ZEMS_SSR_RESOURCE$*/
 import {Configuration} from '../../../../modules/zems/playground/Config/index.mjs'; /*$ZEMS_SSR_RESOURCE$*/
+import {useProperty} from "../../../../modules/zems/core/ZemsReact/index.mjs"; /*$ZEMS_SSR_RESOURCE$*/
 
 export default function PageTitle(props) {
     const {pageTitlePrefix} = Configuration;
-    const {pageTitle} = props;
+    const [pageTitle, setPageTitle] = useProperty('pageTitle', props)
 
     return jsxLight`<h1>${pageTitlePrefix} | ${pageTitle}</h1>`;
 }
