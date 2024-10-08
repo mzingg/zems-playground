713859ffef13453950bb3056005482e96094e3f1
Zingg Engineering <zingg.engineering@gmail.com>
2024-10-08 13:45:58 +0200

    Upgraded gradle

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
